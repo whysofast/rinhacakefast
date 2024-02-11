6684d885b6e2976c3e41d9ecd9c37ccadeb87237
WhySoFast <matheusvca@gmail.com>
2024-02-11 20:24:28 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/rinhacakefast/RinhacakefastApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rinhacakefast
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RinhacakefastApplication
+
+fun main(args: Array<String>) {
+    runApplication<RinhacakefastApplication>(*args)
+}
